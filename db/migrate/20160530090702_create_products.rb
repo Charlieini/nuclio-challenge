bf919c37dae24de82b021c293ef16281adaa4b0c
Carlos <carlos@iniesta.co>
2016-05-30 11:30:31 +0200

    added type model and products controller

--- db/migrate/20160530090702_create_products.rb
+++ db/migrate/20160530090702_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.string :type
+      t.references :type, foreign_key: true, index: true, null: false
       t.decimal :price
 
       t.timestamps null: false
