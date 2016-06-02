c5def9cae8ff8e06345c1413500438cbcf0ff79e
Carlos <carlos@iniesta.co>
2016-06-02 13:46:46 +0200

    lot of improvements

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160530105617) do
+ActiveRecord::Schema.define(version: 20160530112622) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20160530105617) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
